715f84c328e64e4e13a1411d96f711c0410bae6c
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2022-07-20 04:53:38 +0300

    add bastion.tf

---
+++ bastion.tf
@@ -0,0 +1,13 @@
+resource "scaleway_instance_ip" "ip_public" {
+  zone = var.zone
+}
+
+resource "scaleway_instance_server" "bastion" {
+  type              = "DEV1-S"
+  image             = "ubuntu_jammy"
+  tags              = ["bastion", "public"]
+  zone              = var.zone
+  name              = "Bastion"
+  enable_dynamic_ip = "false"
+  ip_id             = scaleway_instance_ip.ip_public.id
+}
