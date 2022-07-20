e91da245335abde26e52baebbf6153bd9642efef
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2022-07-20 04:45:30 +0300

    add provider info and some vars

--- main.tf
+++ main.tf
@@ -6,3 +6,15 @@ terraform {
     }
   }
 }
+
+provider "scaleway" {
+  region     = var.reg
+  project_id = var.project_id
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "scaleway_account_ssh_key" "main" {
+  name       = "main"
+  public_key = var.public_key
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "reg" {
+  description = "Region"
+  default     = "fr-par"
+}
+
+variable "zone" {
+  description = "Zone"
+  default     = "fr-par-1"
+}
+
+variable "public_key" {
+  description = "Public key"
+}
+
+variable "project_id" {
+  description = "Project"
+}
+
+variable "access_key" {
+  description = "access_key"
+}
+
+variable "secret_key" {
+  description = "secret_key"
+}
