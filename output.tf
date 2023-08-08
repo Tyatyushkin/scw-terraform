873d9759297c671337cb93df3bcb223aaf842a86
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2023-08-08 17:33:09 +0300

    add docker.tf

--- output.tf
+++ output.tf
@@ -2,14 +2,6 @@ output "external_ip_addr" {
   value       = scaleway_instance_ip.ip_public.address
   description = "External Bastion IP"
 }
-output "jenkins_ip_addr" {
-  value       = scaleway_instance_ip.ip_jenkins.address
-  description = "External Jenkins IP"
-}
-output "docker_ip_addr" {
-  value       = scaleway_instance_ip.ip_docker.address
-  description = "External Jenkins IP"
-}
 output "bucket_endpoint" {
   value       = scaleway_object_bucket.main.endpoint
   description = "S3 bucket endpoint"
