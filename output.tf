578d90fb0be96b063fe721c1fe90e8e3507b524e
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2024-09-21 19:10:24 +0300

    remove dev server

--- output.tf
+++ output.tf
@@ -3,10 +3,6 @@ output "external_ip_addr" {
   description = "External Bastion IP"
 }
 
-output "dev_ip_add" {
-  value = scaleway_instance_ip.ip_dev.address
-  description = "Dev external IP"
-}
 output "bucket_endpoint" {
   value       = scaleway_object_bucket.main.endpoint
   description = "S3 bucket endpoint"
